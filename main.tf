eaeb54e5afb0989a2094b324b2b3707cc1dcd4a5
Badru <dolapobadru@gmail.com>
2024-01-13 18:24:19 +0100

    #9 add random terraform provider (#10)

--- main.tf
+++ main.tf
@@ -1,2 +1,22 @@
-# This is my first change
-# This is my 2nd change
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+
+resource "random_string" "bucket_name" {
+  length = 16
+  special = false
+}
+
+output "random_bucket_name" {
+    value = random_string.bucket_name.result
+}
